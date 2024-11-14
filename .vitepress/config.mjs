9646e673b37bc9e4e186444001f9061b6c4d2256
ITCOM <159421917+Sysyz-itcom@users.noreply.github.com>
2024-11-15 01:22:28 +0800

    更新 config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -26,6 +26,7 @@ export default defineConfig({
           { text: 'JS 网页组件', link: '/projects/webjs' },
           { text: '软件服务', link: '/projects/software' },
           { text: '浏览器起始页', link: '/projects/startpage' },
+          { text: 'ReadTable', link: '/projects/readtable/readtable' },
         ],
       },
       {
